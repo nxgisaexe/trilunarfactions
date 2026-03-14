acc26ff355d32e01fe191815184cc261c825c806
nxgisaexe <nxgisa.exe05@gmail.com>
2026-03-14 12:18:41 +0800

    fix 404 error lol

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
-
-// https://vite.dev/config/
-
-
 import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/trilunarfactions/",
+  base: '/trilunarfactions/',
   plugins: [
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
-
-
