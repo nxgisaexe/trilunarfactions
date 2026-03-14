ecbb884efd81c70bd3d1ccc6099bc18d0371d3c8
nxgisaexe <nxgisa.exe05@gmail.com>
2026-03-14 21:54:14 +0800

    favicon update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/trilunarfactions/' : '/',
+  base: '/trilunarfactions/',
   plugins: [
     react(),
     tailwindcss(),
@@ -18,4 +18,5 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
   },
+  publicDir: 'public',
 })
